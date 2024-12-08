5db8e09a274775c58b26772c24bae172a694d818
Mohamed Sayed <developper.mohamed@gmail.com>
2024-12-08 21:35:19 +0300

    change logo from roma to tgi

--- resources/views/partials/preloader.blade.php
+++ resources/views/partials/preloader.blade.php
@@ -1,10 +1,9 @@
 <div class="preloader" id="preloader">
     <div class="preloader">
         <div class="waviy position-relative">
-            <span class="d-inline-block">R</span>
-            <span class="d-inline-block">O</span>
-            <span class="d-inline-block">M</span>
-            <span class="d-inline-block">A</span>
+            <span class="d-inline-block">T</span>
+            <span class="d-inline-block">G</span>
+            <span class="d-inline-block">I</span>
 {{--            <span class="d-inline-block">O</span>--}}
         </div>
     </div>
